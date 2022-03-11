3b89f5d57f6e415c2ef9745ce95d6aed47da308e
Dalisalar <65603871+Dalisalar@users.noreply.github.com>
2022-03-11 15:15:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,54 +1,41 @@
-# from telegram import (
-#     Update
-# )
+from telegram import (
+    Update
+)
 
-# from telegram.ext import (
-#     Filters,
-#     Updater,
-#     MessageHandler,
-#     CommandHandler,
-#     CallbackContext
+from telegram.ext import (
+    Filters,
+    Updater,
+    MessageHandler,
+    CommandHandler,
+    CallbackContext
 
-# )
+)
 
 
-# WEBHOOK_URL = "104.197.6.61"
-# WEBHOOK_HOST = "0.0.0.0"
-# WEBHOOK_PORT = "8443"
-# CERT_PATH = "./url_cert.pem"
-# KEY_PATH = "./url_private.key"
+WEBHOOK_URL = "104.197.6.61"
+WEBHOOK_LISTEN = "0.0.0.0"
+WEBHOOK_PORT = "8443"
 
-# API_TOKEN = "5063316173:AAGUZtfr1OQ9K-mY3w_pl81gOpj-6KKKBIg"
-# updater = Updater(API_TOKEN)
-# dp = updater.dispatcher
+CERT_PATH = "./url_cert.pem"
+KEY_PATH = "./url_private.key"
 
-# def start(update: Update, context: CallbackContext):
-#     update.message.reply_text("Hi!")
+API_TOKEN = "5063316173:AAGUZtfr1OQ9K-mY3w_pl81gOpj-6KKKBIg"
+updater = Updater(API_TOKEN)
+dp = updater.dispatcher
 
-# def echo(update: Update, context: CallbackContext):
-#     context.bot.send_message(update.effective_chat.id, update.message.text)
+def start(update: Update, context: CallbackContext):
+    update.message.reply_text("Hi!")
 
-# dp.add_handler(CommandHandler("start", start))
-# dp.add_handler(MessageHandler(Filters.all & ~Filters.command, echo))
+def echo(update: Update, context: CallbackContext):
+    context.bot.send_message(update.effective_chat.id, update.message.text)
 
-# updater.start_webhook(
-#     webhook_url=WEBHOOK_URL,
-#     listen=WEBHOOK_HOST,
-#     port=WEBHOOK_PORT,
-#     cert=CERT_PATH,
-#     key=KEY_PATH
-# )
+dp.add_handler(CommandHandler("start", start))
+dp.add_handler(MessageHandler(Filters.all & ~Filters.command, echo))
 
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/", methods=["POST"])
-def index():
-    print("New request")
-    return "", 200
-
-app.run(
-    host="0.0.0.0",
-    port="8443"
+updater.start_webhook(
+    webhook_url=WEBHOOK_URL,
+    listen=WEBHOOK_LISTEN,
+    port=WEBHOOK_PORT,
+    # cert=CERT_PATH,
+    # key=KEY_PATH
 )
