24b05c05a86e7a91da4452a9d6f6ed701b23a72b
Dalisalar <65603871+Dalisalar@users.noreply.github.com>
2022-03-11 14:40:29 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,40 @@
+from asyncore import dispatcher
+from telegram import (
+    Update
+)
+
+from telegram.ext import (
+    Filters,
+    Updater,
+    MessageHandler,
+    CommandHandler,
+    CallbackContext
+
+)
+
+WEBHOOK_URL = "104.197.6.61"
+WEBHOOK_PORT = "8334"
+CERT_PATH = "./url_cert.pem"
+KEY_PATH = "./url_private.key"
+
+API_TOKEN = "5063316173:AAGUZtfr1OQ9K-mY3w_pl81gOpj-6KKKBIg"
+updater = Updater(API_TOKEN)
+dp = updater.dispatcher
+
+def start(update: Update, context: CallbackContext):
+    update.message.reply_text("Hi!")
+
+def echo(update: Update, context: CallbackContext):
+    context.bot.send_message(update.effective_chat.id, update.message.text)
+
+dp.add_handler(CommandHandler("start", start))
+dp.add_handler(MessageHandler(Filters.all & ~Filters.command, echo))
+
+# updater.start_webhook(
+#     webhook_url=WEBHOOK_URL,
+#     port=WEBHOOK_PORT,
+#     cert=CERT_PATH,
+#     key=KEY_PATH
+# )
+
+updater.start_polling()
