2f9f67e5e9586505df4ab5f62c0188100325fd92
Dalisalar <65603871+Dalisalar@users.noreply.github.com>
2022-03-11 15:35:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -10,10 +10,11 @@ from telegram.ext import (
     CallbackContext
 
 )
+from torch import USE_RTLD_GLOBAL_WITH_LIBTORCH
 
 
 WEBHOOK_URL = "104.197.6.61"
-WEBHOOK_LISTEN = "0.0.0.0/0"
+WEBHOOK_LISTEN = "0.0.0.0"
 WEBHOOK_PORT = "8443"
 
 CERT_PATH = "./url_cert.pem"
@@ -33,9 +34,10 @@ dp.add_handler(CommandHandler("start", start))
 dp.add_handler(MessageHandler(Filters.all & ~Filters.command, echo))
 
 updater.start_webhook(
-    webhook_url=WEBHOOK_URL,
     listen=WEBHOOK_LISTEN,
     port=WEBHOOK_PORT,
+    url_path=API_TOKEN,
+    webhook_url=f"https://{WEBHOOK_URL}:{WEBHOOK_PORT}/{API_TOKEN}",
     cert=CERT_PATH,
     key=KEY_PATH
 )
