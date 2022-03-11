469fe69e8098c1ca6d884bb1d1c7d95d0f5152da
Dalisalar <65603871+Dalisalar@users.noreply.github.com>
2022-03-11 15:28:39 +0300

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ dp.add_handler(CommandHandler("start", start))
 dp.add_handler(MessageHandler(Filters.all & ~Filters.command, echo))
 
 updater.start_webhook(
-    listen=WEBHOOK_URL,
+    webhook_url=WEBHOOK_URL,
     port=WEBHOOK_PORT,
     # cert=CERT_PATH,
     # key=KEY_PATH
